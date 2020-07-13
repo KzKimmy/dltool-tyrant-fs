256012e758f5aa2448a0ea4d56232780ad9ea9f3
KzKimmy <36092808+KzKimmy@users.noreply.github.com>
2020-07-13 11:52:48 +0700

    get all anime w/ many bug xD

--- index.php
+++ index.php
@@ -25,43 +25,22 @@
 
         <?php
 require_once('simple_html_dom.php');
- 
-$domain1 = 'https://tyrantfs.wixsite.com/tyrantfs/seton';
-$domain2 = 'https://tyrantfs.wixsite.com/tyrantfs/nekopara';
-$seton = file_get_html($domain1);
-$nekopara = file_get_html($domain2);
-?>
-<br>
-        <h3 class="title is-3">Murenase! Seton Gakuen</h3>
+$get_project = 'https://tyrantfs.wixsite.com/tyrantfs/projects';
+$dcp_project = file_get_html($get_project);
+$project_count = 1;
+foreach($dcp_project->find('.txtNew a') as $el) {
+    if($el->plaintext != null && $el->href != 'https://tyrantfs.wixsite.com/tyrantfs/projects') {
+        $domain.$project_count = $el->href;
+        $title = file_get_html($domain.$project_count);
+    ?>
+        <h3 class="title is-3"><?php echo $el->plaintext; ?></h3>
         <div class="table-container">
             <table class="table is-hoverable">
                 <tbody>
                     <?php
                     $ep = 1;
-                        foreach($seton->find('.txtNew a') as $el) {
-                        if($el->plaintext != null && $el->plaintext == 'DL') { ?>
-                    <tr>
-                        <td>Episode <?php echo $ep; ?></td>
-                        <td>
-                            <a href="<?php echo $el->href; ?>" target="_blank"><?php echo $el->href; ?></a>
-                        </td>
-                    </tr>
-                    <?php
-                        $ep++;
-                        }
-                    }?>
-                </tbody>
-            </table>
-        </div>
-        <br>
-        <h3 class="title is-3">Nekopara</h3>
-        <div class="table-container">
-            <table class="table is-hoverable">
-                <tbody>
-                    <?php
-                    $ep = 1;
-                        foreach($nekopara->find('.txtNew a') as $el) {
-                        if($el->plaintext != null && $el->plaintext == 'DL') { ?>
+                        foreach($title->find('.txtNew a') as $el) {
+                        if($el->plaintext != null or ($el->plaintext == 'DL' or $el->plaintext == 'MEGA' )) { ?>
                     <tr>
                         <td>Episode <?php echo $ep; ?></td>
                         <td>
@@ -76,10 +55,14 @@ $nekopara = file_get_html($domain2);
             </table>
         </div>
         <?php
-    $seton->clear();
-    $nekopara->clear();
-    unset($seton);
-    unset($nekopara);
+        $project_count++;
+    }
+}
+?>
+    </div>
+    <?php
+    $title->clear();
+    unset($title);
     ?>
     </div>
 </body>
